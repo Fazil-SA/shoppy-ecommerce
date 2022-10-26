5d6232753cf7219f3a1773743818a0ff873f4e44
Fazil-SA <fazilsa2000@gmail.com>
2022-10-26 15:38:55 +0530

    twillio config.env added

--- config/config.js
+++ config/config.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config({path:"config.env"})
+
 module.exports={
 
-    ServiceID:"	VAa5af35dcfbad8488cf5598d1861ad0c3",
-    accountSID:"AC1c764f59f4cff73674e9739d420b4c5f",
-    authTocken:"d08e11b18670924dde2d1a723d48706f"
+    ServiceID: process.env.ServiceID,
+    accountSID: process.env.accountSID,
+    authTocken:process.env.authTocken
 
 }
